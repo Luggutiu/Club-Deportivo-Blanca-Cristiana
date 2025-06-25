77de6ca1c5a72ac3d95516e472d5c660429fefa6
Luggutiu <cuentaunika01@gmail.com>
2025-06-25 02:09:15 -0500

    Vista index cargando publicaciones

--- app/routes/posts.py
+++ app/routes/posts.py
@@ -1,23 +1,16 @@
-from fastapi import APIRouter
 from fastapi import APIRouter, Request
-from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from app.embedder import generar_embed
 from app.database import SessionLocal
 from app import models
-from app.embedder import generar_embed
-
-router = APIRouter()
-templates = Jinja2Templates(directory="app/templates")
-
-router = APIRouter()
-
-from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
 templates = Jinja2Templates(directory="app/templates")
 
 @router.get("/", response_class=HTMLResponse)
 def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    db = SessionLocal()
+    posts = db.query(models.Post).all()
+    db.close()
+    return templates.TemplateResponse("index.html", {"request": request, "posts": posts})
