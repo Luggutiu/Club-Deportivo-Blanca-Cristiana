031c91eaaab0e0d76e06e18e389c69b285748da5
Luggutiu <cuentaunika01@gmail.com>
2025-06-28 15:54:39 -0500

    ult mej bhc5akxsed_dkdkkgghp4ddublicd.html

--- app/utils/email_utils.py
+++ app/utils/email_utils.py
@@ -1,10 +1,11 @@
 from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
 from pydantic import EmailStr
+import os
 
-conf = ConnectionConfig(
-    MAIL_USERNAME="clubdeportivoblancacristiana@gmail.com",
-    MAIL_PASSWORD="mqwghyheyjxlisyh",
-    MAIL_FROM="clubdeportivoblancacristiana@gmail.com",
+conf = ConnectionConfig (
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME"),
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD"),
+    MAIL_FROM = os.getenv("MAIL_FROM"),
     MAIL_PORT=587,
     MAIL_SERVER="smtp.gmail.com",
     MAIL_TLS=True,
